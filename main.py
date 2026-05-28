eac35b927f7dbe1fa01c739f8f8534d0f88ceee1
Hashir Sohail <syedhashirsohail@gmail.com>
2026-05-28 14:19:36 +0200

    Added Create/Read

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks= [
+    {"title":"study Fastapi"},
+    {"title2":"study Fastapi"},
+    {"title":"study Fastapi"}
+]
+
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return {"message": "Task Api"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.post("/tasks")
+def create_task(task:dict):
+    tasks.append(task)
+    return {"message":"task added"}
