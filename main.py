7c95db276f9da5f4d39f6896d746fb0e53568f70
Hashir Sohail <syedhashirsohail@gmail.com>
2026-05-28 15:51:25 +0200

    Added update and Delet

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ tasks= [
         "id":1,
         "title":"learn FastApi",
         "completed":False
+    },
+    {
+        "id":2,
+        "title":"learn FastApi",
+        "completed":False
+    },{
+        "id":3,
+        "title":"learn FastApi",
+        "completed":False
     }
 ]
-task_id_counter=2
+task_id_counter=4
 
 @app.get("/")
 def home():
@@ -30,3 +39,23 @@ def create_task(task:dict):
     tasks.append(new_task)
     task_id_counter +=1
     return new_task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted"}
+
+    return {"error": "Task not found"}
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: dict):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.get("title", task["title"])
+            task["completed"] = updated_task.get("completed", task["completed"])
+            return task
+
+    return {"error": "Task not found"}
